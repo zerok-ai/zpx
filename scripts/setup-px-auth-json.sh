ff509c7c4410dba39088d46497786be0dc76ca57
Avin Goyal <avingoyal01@gmail.com>
2023-02-10 23:07:37 +0530

    script to generate auth.json to bypass pixie login

---
+++ scripts/setup-px-auth-json.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $THIS_DIR/variables.sh
+
+AUTHJSON=$($SCRIPTS_DIR/pixie-ui-cli.sh -c authjson)
+echo $AUTHJSON >> ~/.pixie/auth.json
+# cat ~/.pixie/auth.json
