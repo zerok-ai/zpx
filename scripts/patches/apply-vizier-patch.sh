1b473943baf28be917a81f346f0d2c6c7268cf05
Avin Goyal <avingoyal01@gmail.com>
2023-02-13 10:58:22 +0530

    adding second part of the https - response limit patch

--- scripts/patches/apply-vizier-patch.sh
+++ scripts/patches/apply-vizier-patch.sh
@@ -16,12 +16,19 @@ then
 
         echo 'Applying patch'
 
-        FILE='src/stirling/source_connectors/socket_tracer/protocols/http/parse.cc'
 
         cd $PIXIE_DIR
+        
+        FILE='src/stirling/source_connectors/socket_tracer/protocols/http/parse.cc'
         git checkout $FILE
         perl -pi -e "s/DEFINE_uint32\(http_body_limit_bytes, 1024/DEFINE_uint32\(http_body_limit_bytes, 100*1024/" $FILE
-        cd $ZPX_DIR
         
+        FILE='src/stirling/core/data_table.h'
+        git checkout $FILE
+        perl -pi -e "s/val.resize\(max_string_bytes\);/\/\/val.resize\(max_string_bytes\);/" $FILE
+        perl -pi -e "s/val.append\(kTruncatedMsg\);/\/\/val.append\(kTruncatedMsg\);/" $FILE
+        perl -pi -e "s/val.shrink_to_fit\(\);/\/\/val.shrink_to_fit\(\);/" $FILE
+        cd $ZPX_DIR
+
 fi
 
