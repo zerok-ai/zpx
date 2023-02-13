164537a762a4ce682ce32d11361cfb5f77778808
Avin Goyal <avingoyal01@gmail.com>
2023-02-13 14:32:37 +0530

    one more patch

--- scripts/patches/apply-vizier-patch.sh
+++ scripts/patches/apply-vizier-patch.sh
@@ -14,11 +14,12 @@ PATCH_VIZIER=$retval
 if [ "$PATCH_VIZIER" == '1' ]
 then
 
-        echo 'Applying patch'
-
+        PATCH_PREFIX="\/**ZEROK-STARTS**\/"
+        PATCH_SUFFIX="\/**ZEROK-ENDS**\/"
 
         cd $PIXIE_DIR
         
+        echo "Applying HTTPS response limit patch"
         FILE='src/stirling/source_connectors/socket_tracer/protocols/http/parse.cc'
         git checkout $FILE
         perl -pi -e "s/DEFINE_uint32\(http_body_limit_bytes, 1024/DEFINE_uint32\(http_body_limit_bytes, 100*1024/" $FILE
@@ -28,6 +29,12 @@ then
         perl -pi -e "s/val.resize\(max_string_bytes\);/\/\/val.resize\(max_string_bytes\);/" $FILE
         perl -pi -e "s/val.append\(kTruncatedMsg\);/\/\/val.append\(kTruncatedMsg\);/" $FILE
         perl -pi -e "s/val.shrink_to_fit\(\);/\/\/val.shrink_to_fit\(\);/" $FILE
+        
+        echo "Applying MYSQL results in response patch"
+        FILE='src/stirling/source_connectors/socket_tracer/protocols/mysql/handler.cc'
+        git checkout $FILE
+        perl -pi -e "s/absl::StrAppend\(&entry->resp.msg, \"Resultset rows = \", results.size\(\)\);/\n\tstd::string resultsString = \"\";\n\tfor \(auto & element : results\) {\n\t\tresultsString = resultsString + element.msg + \" | \";\n\t}\n\n\tabsl::StrAppend\(&entry->resp.msg, \"Resultset rows = \", resultsString\);/" $FILE
+        
         cd $ZPX_DIR
 
 fi
