ffa0fcdd1bffeabc19bba5a8ad1748c09e70c41e
Mudit Krishna Mathur <muditkmathur@gmail.com>
2023-03-13 20:36:54 +0530

    Changed default settings for a few variables

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-export ASK_USER=1
+export ASK_USER=0
 export FORCE_ASK_PX_OPERATOR=1
 export SETUP_CLUSTER=1
 
 #Are we going to setup px operator in the same cluster as px host?
 export USE_MKCERT_CA=0
-export SAME_CLUSTER_SETUP=1
+export SAME_CLUSTER_SETUP=0
 export PIXIE_HOST_DEV_MODE=1
 export PIXIE_OPERATOR_DEV_MODE=0
 export PIXIE_VIZIER_DEV_MODE=1
@@ -13,7 +13,11 @@ export PIXIE_REPO=us-west1-docker.pkg.dev/zerok-dev/pixie-dev
 
 #Basic cluster parameters
 export ZONE=us-west1-b
-export CLUSTER_NAME=devpx06
+
+if [[ -z $CLUSTER_NAME ]]; then
+    export CLUSTER_NAME=zkcloud01
+fi
+
 export PX_CLUSTER_NAME=zkproxy-demo
 export PX_CLUSTER_PROJECT=zerok-dev
 export CLUSTER_NUM_NODES=2
