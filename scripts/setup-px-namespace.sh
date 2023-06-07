20f3a918a70c8fc98a8b5d46d607c22ed0aaa99f
DevOps-ZeroK <134274939+DevOps-ZeroK@users.noreply.github.com>
2023-06-07 10:19:29 +0530

    Update setup-px-namespace.sh

--- scripts/setup-px-namespace.sh
+++ scripts/setup-px-namespace.sh
@@ -8,7 +8,9 @@ SETUP_PX_NAMESPACE=$retval
 if [ "$SETUP_PX_NAMESPACE" == '1' ]
 then
     kubectl create namespace plc
-    #$PIXIE_DIR/scripts/create_cloud_secrets.sh
+    echo "creating cloud secrets "
+    $PIXIE_DIR/scripts/create_cloud_secrets.sh
+    echo "done creating cloud secrets "
     # TODO: Create PR for self-hosted-pixie setup
 fi
 
