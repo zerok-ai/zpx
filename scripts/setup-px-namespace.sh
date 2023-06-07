c377192c2362ab486ba0161081289983fb67bb90
DevOps-ZeroK <134274939+DevOps-ZeroK@users.noreply.github.com>
2023-06-07 11:14:43 +0530

    Update setup-px-namespace.sh

--- scripts/setup-px-namespace.sh
+++ scripts/setup-px-namespace.sh
@@ -9,9 +9,8 @@ if [ "$SETUP_PX_NAMESPACE" == '1' ]
 then
     kubectl create namespace plc
     echo "creating cloud secrets "
-    sed -i 's/set -e/set -x/' $PIXIE_DIR/scripts/create_cloud_secrets.sh
-    sed -i 's/urandom tr -dc \'a-zA-Z0-9\' | fold -w 64 | head -n 1/urandom tr -dc \'a-zA-Z0-9\'/' $PIXIE_DIR/scripts/create_cloud_secrets.sh
-    $PIXIE_DIR/scripts/create_cloud_secrets.sh
+    
+    /scripts/create_cloud_secrets.sh
     echo "done creating cloud secrets "
     # TODO: Create PR for self-hosted-pixie setup
 fi
