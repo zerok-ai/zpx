1f19b1e7a0033f235008418bcdb86cc21f33418d
Avin Goyal <avingoyal01@gmail.com>
2023-07-17 15:23:00 +0530

    changing zpixie config

--- scripts/setup-vizier-dev-force.sh
+++ scripts/setup-vizier-dev-force.sh
@@ -3,6 +3,9 @@
 THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $THIS_DIR/variables.sh
 
+$SCRIPTS_DIR/validate-env-variables.sh
+
+echo ''
 echo ''
 echo '-----------------SETTING-UP-ZPIXIE-REPO-----------------'
 
